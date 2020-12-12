2b481e60655a2bbf5a94a70438959992dcc89ff5
Thang Tran <56148507+thangtc987@users.noreply.github.com>
2020-12-12 13:08:59 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,2 @@
+if (apt list -qqi git | grep -q git); then echo GIT is installed.; else sudo apt install git -y; fi
 git clone https://thangtc987@github.com/thangtc987/setup.git && cd setup && bash 0_start.sh
