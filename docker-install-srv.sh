6c235a8c39cca1d5f915eedb1fd71e68be0848c8
Thang Tran <56148507+thangtc987@users.noreply.github.com>
2021-01-23 06:58:51 +0700

    Update docker-install-srv.sh

--- docker-install-srv.sh
+++ docker-install-srv.sh
@@ -1,5 +1,5 @@
 yes | unminimize && 
+apt-get install jq tree git curl -y &&
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - && apt-get install -y nodejs &&
-apt-get install jq tree git -y &&
 npm install -g pnpm typescript &&
 git config --global user.name "t" && git config --global user.email "t@google.com" && git config --global credential.helper store
