6d4d72ce7dcc9f6ab5c1672ab19e40325e598df8
Thang Tran <56148507+thangtc987@users.noreply.github.com>
2021-01-23 06:46:01 +0700

    Update docker-install-srv.sh

--- docker-install-srv.sh
+++ docker-install-srv.sh
@@ -1,2 +1,4 @@
 yes | unminimize && 
-apt install jq, nodejs, tree, git -y
+apt install jq, nodejs, tree, git -y &&
+npm install -g pnpm typescript &&
+git config --global user.name "t" && git config --global user.email "t@google.com" && git config --global credential.helper store
