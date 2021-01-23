e85fb45701c720f3cfa010bf234a611605f598df
Thang Tran <56148507+thangtc987@users.noreply.github.com>
2021-01-23 16:12:37 +0700

    Update docker-install-srv.sh

--- docker-install-srv.sh
+++ docker-install-srv.sh
@@ -2,4 +2,5 @@ yes | unminimize &&
 apt-get install jq tree git curl -y &&
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash - && apt-get install -y nodejs &&
 npm install -g pnpm typescript &&
-git config --global user.name "t" && git config --global user.email "t@google.com" && git config --global credential.helper store
+git config --global user.name "t" && git config --global user.email "t@google.com" && git config --global credential.helper store &&
+(wget -O- -q https://github.com/thangtc987/setup-public/raw/main/apply-timezone.sh | bash)
