4adc09fc05e0717a4872dcd0293af8d654413f0b
t@local <t@local>
2021-01-23 16:44:57 +0700

    fix git

--- apply-git.sh
+++ apply-git.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-git config --global user.name "t" && git config --global user.email "t@google.com" && git config --global credential.helper store &&
+git config --global user.name "t" && git config --global user.email "t@google.com" && git config --global credential.helper store
