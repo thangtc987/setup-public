f419d11a733949f1be87601b87a7ca4cd83a4584
t@local <t@local>
2021-01-28 17:22:25 +0700

    temp

--- install-helper.sh
+++ install-helper.sh
@@ -3,4 +3,4 @@
 # run below to install
 # wget -O- https://raw.githubusercontent.com/thangtc987/setup-public/main/install-helper.sh | bash
 
-wget -nv https://raw.githubusercontent.com/thangtc987/setup-public/main/x.sh && sudo install x.sh /usr/bin/x && && x
+wget -nv https://raw.githubusercontent.com/thangtc987/setup-public/main/x.sh && sudo install x.sh /usr/bin/x && x
