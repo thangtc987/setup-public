d782f05ce2636713d6923986b14920e4b3bb3517
t@local <t@local>
2021-01-28 16:33:45 +0700

    init x.sh

---
+++ install-helper.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# run below to install
+# wget -O- https://raw.githubusercontent.com/thangtc987/setup-public/main/install-helper.sh | bash
+
+wget -O- https://raw.githubusercontent.com/thangtc987/setup-public/main/x.sh && install x.sh && rm x.sh && x
