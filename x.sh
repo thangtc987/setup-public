1752ee55cf3a8618d8f83dfc408a2bab63140d31
t@local <t@local>
2021-01-28 17:12:03 +0700

    temp

--- x.sh
+++ x.sh
@@ -2,6 +2,15 @@
 
 echo Version: 0.1
 
-if [[ "$1" == "api" ]]; then
-    echo "123 $2"
+if [[ "$1" == "help" ]]; then
+    cat <<EOF 
+USAGE: 
+    x help          Print this help
+    x upgrade       Reinstall this app
+    x <command>     Run API on the setup-public repo
+EOF
+elif [[ "$1" == "upgrade" ]]; then
+    wget -O- https://raw.githubusercontent.com/thangtc987/setup-public/main/install-helper.sh | bash
+else
+    wget -O- https://raw.githubusercontent.com/thangtc987/setup-public/main/$2.sh | bash
 fi
