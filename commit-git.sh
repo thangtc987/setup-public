1cc41b95c89258e298e2ba76570bbf1a97acd15e
Thang Tran <56148507+thangtc987@users.noreply.github.com>
2021-02-14 03:23:06 +0000

    Update commit-git.sh

--- commit-git.sh
+++ commit-git.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-git config --global user.name "t" && git config --global user.email "t@google.com" && git config --global credential.helper store
+git add -A && git commit -a -m "$1-(blank)" && git push
