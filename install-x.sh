4f2386901e94428ab7233cb525b953c7571c398f
Thang Tran <56148507+thangtc987@users.noreply.github.com>
2021-02-27 17:57:41 +0700

    Update install-x.sh

--- install-x.sh
+++ install-x.sh
@@ -4,3 +4,6 @@
 # wget -nv -O- https://raw.githubusercontent.com/thangtc987/setup-public/main/install-x.sh | bash
 
 wget -nv https://raw.githubusercontent.com/thangtc987/setup-public/main/x.sh && sudo install x.sh /usr/bin/x && x
+
+# remove old file
+rm x.sh
