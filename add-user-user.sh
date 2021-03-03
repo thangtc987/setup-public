0eb7f15073ebadb7348c874d90d442ae1a4db232
Thang Tran <56148507+thangtc987@users.noreply.github.com>
2021-03-03 15:23:52 +0700

    Update add-user-user.sh

--- add-user-user.sh
+++ add-user-user.sh
@@ -1,1 +1,1 @@
-(p_name=user && mkdir -p /home/$p_name && useradd $p_name -d /home/$p_name -s /bin/bash && echo "$p_name:$p_name" | chpasswd && usermod -aG sudo $p_name && chmod -R 777 /home/$p_name && chown $p_name /home/$p_name)
+(p_name=user && mkdir -p /home/$p_name && useradd $p_name -d /home/$p_name -s /bin/bash && echo "$p_name:$p_name" | chpasswd && usermod -aG sudo $p_name && chmod -R 777 /home/$p_name && chown -R $p_name /home/$p_name)
